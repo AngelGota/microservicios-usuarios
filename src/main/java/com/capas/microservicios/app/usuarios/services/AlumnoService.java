4ea0709c2d28a68fa699fe8ecf4a617cf8acf093
Angelo <gangelos630@gmail.com>
2024-03-15 19:45:35 -0500

    modificando controller y services con la libreia generica creada

--- src/main/java/com/capas/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/capas/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,17 +1,8 @@
 package com.capas.microservicios.app.usuarios.services;
 
-import java.util.Optional;
-
+import com.capas.microservicios.app.commons.services.CommonService;
 import com.capas.microservicios.app.usuarios.models.entity.Alumno;
 
-public interface AlumnoService {
-	
-	public Iterable<Alumno> findAll();
-	
-	public Optional<Alumno> findById(Long id);
-	
-	public Alumno save(Alumno alumno);
-	
-	public void deleteById(Long id);
+public interface AlumnoService extends CommonService<Alumno>{
 	
 }
--- src/main/java/com/capas/microservicios/app/usuarios/services/AlumnoServiceImplement.java
+++ src/main/java/com/capas/microservicios/app/usuarios/services/AlumnoServiceImplement.java
@@ -1,42 +1,13 @@
 package com.capas.microservicios.app.usuarios.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.capas.microservicios.app.commons.services.CommonServiceImplement;
 import com.capas.microservicios.app.usuarios.models.entity.Alumno;
 import com.capas.microservicios.app.usuarios.models.repository.AlumnoRepository;
 
 @Service
-public class AlumnoServiceImplement implements AlumnoService {
+public class AlumnoServiceImplement extends CommonServiceImplement<Alumno, AlumnoRepository> implements AlumnoService {
 
-	@Autowired
-	private AlumnoRepository repository;
-	
-	@Override
-	@Transactional(readOnly = true)
-	public Iterable<Alumno> findAll() { 
-		return repository.findAll();
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Optional<Alumno> findById(Long id) { 
-		return repository.findById(id);
-	}
-
-	@Override
-	@Transactional
-	public Alumno save(Alumno alumno) { 
-		return repository.save(alumno);
-	}
-
-	@Override
-	@Transactional
-	public void deleteById(Long id) { 
-		repository.deleteById(id);
-	}
 
 }
